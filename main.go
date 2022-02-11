a5831aeb21ac72c9bc4b558832d1388b7e280532
matthiass2 <matias_69914081@hotmail.com>
2022-02-10 23:56:19 -0300

    feat: add controlles and Database(json)

--- main.go
+++ main.go
@@ -20,8 +20,18 @@ type Address struct {
 	State string `json:"state,omitempty"`
 }
 
+func HandleFuncPerson() {
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+}
+
+var people []Person
+
 // função principal
 func main() {
+	HandleFuncPerson()
+
 	router := mux.NewRouter()
 	router.HandleFunc("/contato", GetPeople).Methods("GET")
 	router.HandleFunc("/contato/{id}", GetPerson).Methods("GET")
@@ -33,14 +43,33 @@ func main() {
 
 func GetPeople(w http.ResponseWriter, r *http.Request) {
 
-	var people []Person
-
-	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
-
 	json.NewEncoder(w).Encode(people)
 }
-func GetPerson(w http.ResponseWriter, r *http.Request)    {}
-func CreatePerson(w http.ResponseWriter, r *http.Request) {}
-func DeletePerson(w http.ResponseWriter, r *http.Request) {}
+
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
+}
